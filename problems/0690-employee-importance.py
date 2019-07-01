8808d420f4a7db41af1e4c0bebf1d22d8f17bc49
tzxyz <tz.zhuo@protonmail.com>
2019-07-01 17:54:23 +0800

    :white_check_mark: solved 0690-employee-importance

---
+++ problems/0690-employee-importance.py
@@ -0,0 +1,54 @@
+class Employee:
+    def __init__(self, id, importance, subordinates):
+        # It's the unique id of each node.
+        # unique id of this employee
+        self.id = id
+        # the importance value of this employee
+        self.importance = importance
+        # the id of direct subordinates
+        self.subordinates = subordinates
+
+
+class Solution:
+    """
+    给定一个保存员工信息的数据结构，它包含了员工唯一的id，重要度 和 直系下属的id。
+    比如，员工1是员工2的领导，员工2是员工3的领导。他们相应的重要度为15, 10, 5。那么员工1的数据结构是[1, 15, [2]]，员工2的数据结构是[2, 10, [3]]，员工3的数据结构是[3, 5, []]。注意虽然员工3也是员工1的一个下属，但是由于并不是直系下属，因此没有体现在员工1的数据结构中。
+    现在输入一个公司的所有员工信息，以及单个员工id，返回这个员工和他所有下属的重要度之和。
+
+    示例 1:
+
+        输入: [[1, 5, [2, 3]], [2, 3, []], [3, 3, []]], 1
+        输出: 11
+
+    解释:
+
+        员工1自身的重要度是5，他有两个直系下属2和3，而且2和3的重要度均为3。因此员工1的总重要度是 5 + 3 + 3 = 11。
+
+    注意:
+
+        一个员工最多有一个直系领导，但是可以有多个直系下属
+        员工数量不超过2000。
+
+    来源：力扣（LeetCode）
+    链接：https://leetcode-cn.com/problems/employee-importance
+    著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+    """
+    def getImportance(self, employees, id):
+        """
+        使用递归
+        """
+        d = {}
+        for employee in employees:
+            d[employee.id] = employee
+
+        def score(employee: Employee):
+            # 员工分数 + 下属员工分数
+            return employee.importance + sum([score(d[sid]) for sid in employee.subordinates])
+
+        return score(d[id])
+
+
+if __name__ == '__main__':
+    # employees = [[1, 5, [2, 3]], [2, 3, []], [3, 3, []]]
+    employees = [[1, 2, [2]], [2, 3, []]]
+    print(Solution().getImportance(employees, 2))
